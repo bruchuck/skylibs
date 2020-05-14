9b46a9175957e220c70b406630bbaf832eb20fee
Bruno <brunodortamarques@gmail.com>
2020-05-14 19:18:14 -0300

    conda installs requirement on setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 import os
 import sys
-from setuptools import setup
+import conda.cli.python_api as Conda
 
+
+from setuptools import setup
+version_string =  str(sys.version_info.major) + str(sys.version_info.minor)
 
 if os.name == 'nt':
     extra_requires = ['cffi>=1.9.1']
     dependency_links = []
-    package_data = {"sh": ["libsh.cp36-win_amd64.pyd"],
-                    "tools3d": ["libspharm.cp36-win_amd64.pyd"]}
+    package_data = {"sh": ["libsh.cp" + version_string +"-win_amd64.pyd"],
+                    "tools3d": ["libspharm.cp" + version_string + "-win_amd64.pyd"]}
 else:
     extra_requires = ['openexr>=1.3.0']
     dependency_links = ['https://github.com/jamesbowman/openexrpython/tarball/master#egg=openexr-1.3.0']
-    if sys.version_info.major == 3:
-        if sys.version_info.minor == 5:
-            package_data = {"sh": ["libsh.cpython-35m-x86_64-linux-gnu.so"],
-                    "tools3d": ["libspharm.cpython-35m-x86_64-linux-gnu.so"]}
-        else:
-            package_data = {"sh": ["libsh.cpython-37m-x86_64-linux-gnu.so"],
-                    "tools3d": ["libspharm.cpython-37m-x86_64-linux-gnu.so"]}
+    package_data = {"sh": ["libsh.cpython-" + version_string + "m-x86_64-linux-gnu.so"], 
+                    "tools3d": ["libspharm.cpython-"+ version_string +"m-x86_64-linux-gnu.so"]}
+
+
+conda_install_packages = ['numpy', 'scipy', 'imageio', 'tqdm', 'cffi', 'astropy', 'pandas', 'xarray', 'matplotlib']
+out = Conda.run_command("install", conda_install_packages)
 
 setup(
     name='skylibs',
     description=('Tools to read, write, perform projections and handle LDR/HDR environment maps (IBL).'),
-    author='Yannick Hold',
-    author_email='yannickhold@gmail.com',
+    author='Bruno Marques, Original author: Yannick Hold',
+    author_email='chuckof@gmail.com',
     license="LGPLv3",
-    url='https://github.com/soravux/skylibs',
-    version='0.4.1',
-    packages=['ezexr', 'envmap', 'hdrio', 'hdrtools', 'hdrtools/tonemapping', 'sh', 'skydb', 'tools3d'],
+    url='https://github.com/bdorta/skylibs',
+    version='0.5',
+    install_requires=['rotlib', 'pyshtools'],
     package_data=package_data,
     include_package_data=True,
-    install_requires=['imageio>=1.6', 'rotlib>=0.91', 'tqdm', 'pyshtools'].extend(extra_requires),
-    dependency_links=dependency_links,
+    cffi_modules=["sh/build_sh.py:ffibuilder"]
 )
 
--- sh/build_sh.py
+++ sh/build_sh.py
@@ -1,6 +1,12 @@
 from cffi import FFI
+import os
 
+
+#os.environ['CL'] = '-I$(pwd)/src/'
 ffibuilder = FFI()
+
+
+print(os.environ)
 
 # For every function that you want to have a python binding,
 # specify its declaration here
@@ -8,11 +14,11 @@ ffibuilder.cdef("""
     void ySH(const float * normals, double *data_out, const unsigned int height, const unsigned int width, const int l, const int m);
                 """)
 
+
 # Here go the sources, most likely only includes and additional functions if necessary
 ffibuilder.set_source("libsh",
-    """
-    #include "sh.h"
-    """, sources=["sh.c"])
+    None, sources=["src/sh.c"])
+
 
 if __name__ == "__main__":
     ffibuilder.compile()
