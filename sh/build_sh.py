1c8b07f43c5099da7723245991b6aaaef6ceff75
Bruno <brunodortamarques@gmail.com>
2020-05-15 13:12:39 -0300

    add skylibs path to sh module includes

--- sh/build_sh.py
+++ sh/build_sh.py
@@ -1,19 +1,13 @@
 from cffi import FFI
 import os
 
-
-
 ffibuilder = FFI()
-
-
-print(os.environ)
 
 # For every function that you want to have a python binding,
 # specify its declaration here
 ffibuilder.cdef(r"""
     void ySH(const float * normals, double *data_out, const unsigned int height, const unsigned int width, const int l, const int m);
                 """)
-
 
 
 source_dir = os.path.join(os.getcwd(), "sh", "src")
@@ -24,7 +18,7 @@ ffibuilder.set_source("_libsh",
     """,
     sources=[os.path.join(source_dir, "sh.c")],
     relative_to=__file__,
-    include_dirs = [source_dir]
+    include_dirs = [source_dir, os.path.join(source_dir, "skylibs")]
 )
 
 
